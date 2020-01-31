ebf430d27d7452c2d21bdfd016762086f0287e32
Manfredi Soldano <manfredisoldano@outlook.com>
2020-02-01 00:24:47 +0100

    Included SpawnManager
    -Spawn Manager
    -Basic GameController
    -Included Tag

--- Assets/Script/Controller/GameController/GameController.cs
+++ Assets/Script/Controller/GameController/GameController.cs
@@ -1,8 +1,42 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
-   
+    public List<PlayerManager> playersList = new List<PlayerManager>();
+    public int numberOfFixers;
+    public int numberOfDestroyers;
+    public SpawnManager spawnManager;
+    public GameObject playerContainer;
+
+
+    public void Start()
+    {
+        SpawnInitialPlayers();
+    }
+
+    /// <summary>
+    /// Spawn the numeber of players defined from the number of players value.
+    /// </summary>
+    private void SpawnInitialPlayers()
+    {
+        while (numberOfFixers > 0)
+        {
+            GameObject player = spawnManager.SpawnNewPlayer(PlayerType.FIXER);
+            player.transform.parent = playerContainer.transform;
+            playersList.Add(player.GetComponent<PlayerManager>());
+            numberOfFixers--;
+        }
+
+        while (numberOfDestroyers > 0)
+        {
+            GameObject player = spawnManager.SpawnNewPlayer(PlayerType.DESTROYER);
+            player.transform.parent = playerContainer.transform;
+            playersList.Add(player.GetComponent<PlayerManager>());
+            numberOfDestroyers--;
+        }
+
+    }
 }
