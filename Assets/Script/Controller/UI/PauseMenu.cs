c1049181cda4dc20929e6e0924cb3628528d8114
Manfredi Soldano <manfredisoldano@outlook.com>
2020-02-02 01:27:16 +0100

    Update pause menu

--- Assets/Script/Controller/UI/PauseMenu.cs
+++ Assets/Script/Controller/UI/PauseMenu.cs
@@ -65,7 +65,7 @@ public class PauseMenu : MonoBehaviour
     {
         if (!blockLoading)
         {
-            SceneManager.LoadScene(0, LoadSceneMode.Single);
+            SceneManager.LoadScene(1, LoadSceneMode.Single);
             blockLoading = true;
         }
     }
